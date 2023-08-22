75dc1232f88a22934c5a2c9969bdef8bd6377795
CodeWithRodi <contact@codewithrodi.com>
2023-08-22 01:27:52 -0400

    Support for Docker and Screenshots folder.

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0'
+  }
 });
